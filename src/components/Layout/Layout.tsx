b8947ecc00bc1a81facccdd9145a2de62a361ad9
hardede <dimakostyuk50@gmail.com>
2022-07-12 19:54:19 +0300

    some fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children, theme }) => {
   return (
     <div className={theme === "light" ? "theme-light" : "theme-dark"}>
-      <div className="p-10 h-screen xs:p-5 bg-skin-base transition_forAll md:h-full">
+      <div className="p-10 h-screen xs:p-5 bg-skin-base transition_forAll">
         {children}
       </div>
     </div>
